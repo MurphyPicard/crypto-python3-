04ad8f9fdda8dce6e16efa2ce75d8c562a32e9d1
murphypicard <ayaghsizian@gmail.com>
2016-12-31 21:19:59 -0500

    added command line arguments and validation to caesar

--- caesar.py
+++ caesar.py
@@ -1,4 +1,8 @@
+# I wrote these two functions in another file to stay DRY
 from helpers import alphabet_position, rotate_character
+
+# This is so the user can write a number in the command line
+from sys import argv, exit
 
 # takes a string and rotates each letter and returns an encrypted string
 def encrypt(text, rot):
@@ -7,7 +11,19 @@ def encrypt(text, rot):
         scrambled_text += rotate_character(letter, rot)
     return scrambled_text
 
-a_message = input("Type a message: ")
-a_number = int(input("Rotate by: "))
+# did the user put a number in the command line?
+def user_input_is_valid(cl_args):
+    if len(cl_args) == 2 and cl_args[1].isdigit(): # exactly 2 arguments needed
+        return True
+    print("Please enter a number in the command line like 'python3 caesar.py 23'")
+    exit()
+    return False
 
-print(encrypt(a_message, a_number))
+user_input_is_valid(argv) # did they put a number in the command line?
+
+a_message = input("Type a message: ")
+# a_number = int(input("Rotate by: "))
+# instead of asking for a number, the user will type a number on the command line
+
+# argv[1] is the number the user types in the command line
+print(encrypt(a_message, int(argv[1])))
